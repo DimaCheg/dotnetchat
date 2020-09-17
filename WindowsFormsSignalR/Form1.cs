43041416c6a5a63241fcea93e055affafae87999
JumpyL1on <yaschok315@gmail.com>
2020-09-17 20:05:20 +0300

    it looks perfect

--- WindowsFormsSignalR/Form1.cs
+++ WindowsFormsSignalR/Form1.cs
@@ -16,7 +16,9 @@ namespace WindowsFormsSignalR
         private async void button1_Click(object sender, EventArgs e0)
         {
             HubConnection = new HubConnectionBuilder().WithUrl("http://localhost:5000/chatHub").Build();
-            HubConnection.On("ReceiveMessage", (string name, string message) => textBox2.AppendText(message + "\n"));
+            HubConnection.On("ReceiveMessage",
+                (string name, string message) =>
+                    textBox2.AppendText(name + " says " + message + Environment.NewLine));
             await HubConnection.StartAsync();
             button1.Enabled = false;
             button3.Enabled = true;
@@ -24,12 +26,12 @@ namespace WindowsFormsSignalR
 
         private void button2_Click(object sender, EventArgs e0)
         {
-            HubConnection.InvokeAsync("SendMessage", "Yasha",textBox1.Text);
+            HubConnection.InvokeAsync("SendMessage", "Yasha", textBox1.Text);
         }
 
-        private void button3_Click(object sender, EventArgs e0)
+        private async void button3_Click(object sender, EventArgs e0)
         {
-            HubConnection.DisposeAsync().Wait();
+            await HubConnection.DisposeAsync();
             button3.Enabled = false;
             button1.Enabled = true;
         }
