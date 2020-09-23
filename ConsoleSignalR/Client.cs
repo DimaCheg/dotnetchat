174773258dba71757d4d70109b487dc7b27485a9
Mimahmetova_Albina_11-808 <wildFlow14@gmail.com>
2020-09-23 19:20:31 +0300

    add console client

---
+++ ConsoleSignalR/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ConsoleSignalR
+{
+    class Client
+    {
+        private string name = "unknown";
+        private string url = "https://localhost:44384/chatHub";
+        public HubConnection HubConnection;
+        private bool isConnect = false;
+
+        public Client(){}
+
+        public void Name(string name)
+        {
+            this.name = name;
+        }
+
+        public async void Connect()
+        {
+            if (isConnect) return;
+            HubConnection = new HubConnectionBuilder().WithUrl(url).Build();
+            isConnect = true;
+            HubConnection.On("ReceiveMessage",
+                (string name, string message) =>
+                    Console.WriteLine(name + " says " + message + Environment.NewLine));
+            await HubConnection.StartAsync();
+        }
+
+        public void Send(string message)
+        {
+            HubConnection.InvokeAsync("SendMessage", name, message);
+        }
+
+        public async void Disconnect()
+        {
+            if (!isConnect) return;
+            await HubConnection.DisposeAsync();
+            isConnect = false;
+        }
+    }
+}
---
+++ ConsoleSignalR/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
+
+namespace ConsoleSignalR
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var client = new Client();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var isbreak = false;
+                switch (input)
+                {
+                    case "/e":
+                        client.Disconnect(); 
+                        isbreak = true;
+                        break;
+                    case "/c":
+                        client.Connect();
+                        break;
+                    case "/h":
+                        Help();
+                        break;
+                    case "/d":
+                        client.Disconnect();
+                        break;
+                    default:
+                        if (input.StartsWith("/n"))
+                            client.Name(input.Substring(3));
+                        else client.Send(input);
+                        break;
+                }
+                if (isbreak) break;
+            }
+        }
+
+        static public void Help()
+        {
+            Console.WriteLine("command list:\n/h help\n/e exit\n/c connect\n/d disconnect\n/n change name");
+        }
+    }
+}
